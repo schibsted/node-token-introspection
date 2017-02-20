61272aafe24917e3d1c13e320176e5c1f4113edc
Joakim Wånggren <joakim.wanggren@schibsted.com>
2017-02-20 13:55:38 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# node-token-introspection
+
+Node token introspection package introspects a token towards an oauth service that follows the [RFC 7762](https://tools.ietf.org/html/rfc7662).
+Calling the introspect function will return a promise.
+
+## Install
+
+```bash
+npm install token-introspection --save
+```
+
+## Usage
+
+Introspect package is configured with endpoint and client credentials, and a function is returned.
+Calling that function with token, and optional token_type_hint will return a
+[Promise](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise).
+
+```javascript
+const tokenIntrospection = require('token-introspection')({
+    endpoint: 'https://example.com/introspect',
+    client_id: '<Client ID>',
+    client_secret: '<Client Secret>',
+});
+
+tokenIntrospection(token).then(console.log).catch(console.warn);
+```
+
+## Configuration
+
+| Field         | Required | Comment |
+| ------------- | :------: | ------- |
+| endpoint      | X        | Url to call |
+| client_id     |          | Client ID used to introspect |
+| client_secret |          | Client secret used to introspect |
---
+++ index.js
@@ -0,0 +1,45 @@
+const request = require('request-promise');
+
+function tokenIntrospect(opts) {
+
+    const defaults = {
+        endpoint: '',
+        client_id: '',
+        client_secret: ''
+    };
+
+    const options = Object.assign({}, defaults, opts);
+
+    if (!options.endpoint) {
+        throw new Error('Endpoint is missing from configuration');
+    }
+
+    return function introspect(token, tokenTypeHint) {
+        const form = { token: token };
+        if (tokenTypeHint) {
+            form.token_type_hint = tokenTypeHint;
+        }
+        return request({
+            method: 'POST',
+            url: options.endpoint,
+            form: form,
+            json: true,
+            headers: {
+                Authorization: 'Basic ' + new Buffer(`${options.clientId}:${options.clientSecret}`).toString('base64'),
+                'User-Agent': 'admin-service'
+            }
+        })
+        .then(data => {
+            if (data.active === true) {
+                return data;
+            } else {
+                throw new Error('Token is not active');
+            }
+        })
+        .catch(err => {
+            throw err;
+        });
+    }
+}
+
+module.exports = tokenIntrospect;
