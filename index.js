a9d35324fed01fe4dbede59f6a388bce2978cc44
Joakim Wånggren <joakim.wanggren@schibsted.com>
2017-04-25 16:59:04 +0200

    added eslint

--- index.js
+++ index.js
@@ -1,64 +1,62 @@
 const formEncode = require('form-urlencoded');
 
 function tokenIntrospect(opts = {}) {
+  const defaults = {
+    endpoint: '',
+    client_id: '',
+    client_secret: '',
+    user_agent: 'token-introspection',
+    proxy: '',
+    fetch: null,
+  };
 
-    const defaults = {
-        endpoint: '',
-        client_id: '',
-        client_secret: '',
-        user_agent: 'token-introspection',
-        proxy: '',
-        fetch: null
-    };
+  const options = Object.assign({}, defaults, opts);
 
-    const options = Object.assign({}, defaults, opts);
+  if (!options.endpoint) {
+    throw new Error('Endpoint is missing from configuration');
+  }
 
-    if (!options.endpoint) {
-        throw new Error('Endpoint is missing from configuration');
+  if (!options.fetch) {
+    options.fetch = require('node-fetch');
+  }
+
+  let proxy = null;
+  if (options.proxy) {
+    try {
+      const HttpsProxy = require('https-proxy-agent');
+      proxy = new HttpsProxy(options.proxy);
+    } catch (e) {
+      throw new Error('Proxy url given, but not installed https-proxy-agent package');
+    }
+  }
+
+  return function introspect(token, tokenTypeHint) {
+    const data = { token };
+    if (tokenTypeHint) {
+      data.token_type_hint = tokenTypeHint;
     }
 
-    if (!options.fetch) {
-        options.fetch = require('node-fetch');
-    }
-
-    let proxy = null;
-    if (options.proxy) {
-        try {
-            const HttpsProxy = require('https-proxy-agent');
-            proxy = new HttpsProxy(options.proxy);
-        } catch (e) {
-            throw new Error('Proxy url given, but not installed https-proxy-agent package');
-        }
-    }
-
-    return function introspect(token, tokenTypeHint) {
-        data = { token };
-        if (tokenTypeHint) {
-            data['token_type_hint'] = tokenTypeHint;
-        }
-
-        return options.fetch(options.endpoint, {
-            method: 'POST',
-            body: formEncode(data),
-            headers: {
-                Authorization: 'Basic ' + new Buffer(`${options.client_id}:${options.client_secret}`).toString('base64'),
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'User-Agent': options.user_agent
-            },
-            agent: proxy
-        })
+    return options.fetch(options.endpoint, {
+      method: 'POST',
+      body: formEncode(data),
+      headers: {
+        Authorization: `Basic ${new Buffer(`${options.client_id}:${options.client_secret}`).toString('base64')}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent': options.user_agent,
+      },
+      agent: proxy,
+    })
         .then(res => res.json())
-        .then(data => {
-            if (data.active === true) {
-                return data;
-            } else {
-                throw new Error('Token is not active');
-            }
+        .then((tokenData) => {
+          if (tokenData.active === true) {
+            return tokenData;
+          }
+          throw new Error('Token is not active');
         })
-        .catch(err => {
-            throw err;
+        .catch((err) => {
+          throw err;
         });
-    }
+  };
 }
 
 module.exports = tokenIntrospect;
