4275a8e2e141e4dbc01e24e63aefd90d1c15a22d
Samuel Gulliksson <samuel.gulliksson@schibsted.com>
2017-07-07 13:36:19 +0200

    Support local introspection with static or remote JWKS.

--- index.js
+++ index.js
@@ -1,8 +1,106 @@
 const formEncode = require('form-urlencoded');
+const util = require('util');
+const debug = require('debug')('token-introspection');
+const JwksClient = require('jwks-rsa');
+const jwt = require('jsonwebtoken');
+const jwk2pem = require('pem-jwk').jwk2pem;
+
+const jwtVerify = util.promisify(jwt.verify);
+
+async function localIntrospect(keys, allowedAlgorithms, token, tokenTypeHint) {
+  function findCandidateKeys(jwtHeader) {
+    function alg2keyType(alg) {
+      if (alg.startsWith('HS')) {
+        return 'oct';
+      } else if (alg.startsWith('RS') || alg.startsWith('PS')) {
+        return 'RSA';
+      } else if (alg.startsWith('ES')) {
+        return 'EC';
+      }
+      return null;
+    }
+
+    const filteredKeys = keys.slice()
+      .filter(key => key.kty && key.kty === alg2keyType(jwtHeader.alg));
+    debug('Filtered keys for \'%s\', found %d', jwtHeader.alg, filteredKeys.length);
+
+    if (jwtHeader.kid) {
+      const keyWithKeyId = filteredKeys.find(key => key.kid === jwtHeader.kid);
+      if (keyWithKeyId) {
+        debug('Found key for key id %s', jwtHeader.kid);
+        return [keyWithKeyId];
+      }
+      debug('No key found for key id %s', jwtHeader.kid);
+      return [];
+    }
+
+    return filteredKeys;
+  }
+
+  if (tokenTypeHint !== 'access_token') {
+    debug('Not an access token, tokenTypeHint=%s', tokenTypeHint);
+    throw new Error('Only access tokens are supported for local introspection');
+  }
+
+  const decodedToken = jwt.decode(token, { complete: true });
+  if (!decodedToken) {
+    debug('Not a JWT token');
+    throw new Error('Token is not a JWT');
+  }
+
+  const possibleVerificationKeys = findCandidateKeys(decodedToken.header)
+    .map(jwk2pem);
+
+  /* eslint-disable no-restricted-syntax, no-await-in-loop */
+  for (const key of possibleVerificationKeys) {
+    try {
+      const verified = await jwtVerify(token, key, { algorithms: allowedAlgorithms });
+      return Object.assign({ active: true }, verified);
+    } catch (err) {
+      if (err instanceof jwt.TokenExpiredError) {
+        throw new Error('Token has expired');
+      }
+    }
+  }
+  /* eslint-enable */
+
+  throw new Error('Could not verify token with any key');
+}
+
+function remoteIntrospect(requestInfo, token, tokenTypeHint) {
+  const data = { token };
+  if (tokenTypeHint) {
+    data.token_type_hint = tokenTypeHint;
+  }
+
+  return requestInfo.fetch(requestInfo.endpoint, {
+    method: 'POST',
+    body: formEncode(data),
+    headers: {
+      Authorization: `Basic ${new Buffer(`${requestInfo.client_id}:${requestInfo.client_secret}`).toString('base64')}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'User-Agent': requestInfo.user_agent,
+    },
+    agent: requestInfo.proxy,
+  })
+    .then(res => res.json())
+    .then((tokenData) => {
+      if (tokenData.active === true) {
+        return tokenData;
+      }
+      throw new Error('Token is not active');
+    })
+    .catch((err) => {
+      throw err;
+    });
+}
 
 function tokenIntrospect(opts = {}) {
   const defaults = {
+    jwks: null,
+    jwks_uri: '',
     endpoint: '',
+    allowed_algs: ['RS256'],
     client_id: '',
     client_secret: '',
     user_agent: 'token-introspection',
@@ -12,11 +110,11 @@ function tokenIntrospect(opts = {}) {
 
   const options = Object.assign({}, defaults, opts);
 
-  if (!options.endpoint) {
-    throw new Error('Endpoint is missing from configuration');
+  if (!options.jwks && !options.jwks_uri && !options.endpoint) {
+    throw new Error('Static JWKS, a JWKS URI or introspection endpoint must be specified in the configuration');
   }
 
-  if (!options.fetch) {
+  if (options.jwks_uri && !options.fetch) {
     options.fetch = require('node-fetch');
   }
 
@@ -30,32 +128,49 @@ function tokenIntrospect(opts = {}) {
     }
   }
 
-  return function introspect(token, tokenTypeHint) {
-    const data = { token };
-    if (tokenTypeHint) {
-      data.token_type_hint = tokenTypeHint;
+  const remoteIntrospectionInfo = Object.assign({}, options, { proxy });
+  let fetchJwks = null;
+  if (options.jwks_uri) {
+    const client = new JwksClient({
+      cache: true,
+      rateLimit: true,
+      jwksUri: options.jwks_uri,
+    });
+    fetchJwks = util.promisify(client.getKeys).bind(client);
+  }
+
+  return async function introspect(token, tokenTypeHint) {
+    async function tryLocalIntrospect(keys) {
+      try {
+        return await localIntrospect(keys, options.allowed_algs, token, tokenTypeHint);
+      } catch (err) {
+        debug('Could not verify token: %s', err.message);
+        return null;
+      }
     }
 
-    return options.fetch(options.endpoint, {
-      method: 'POST',
-      body: formEncode(data),
-      headers: {
-        Authorization: `Basic ${new Buffer(`${options.client_id}:${options.client_secret}`).toString('base64')}`,
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'User-Agent': options.user_agent,
-      },
-      agent: proxy,
-    })
-        .then(res => res.json())
-        .then((tokenData) => {
-          if (tokenData.active === true) {
-            return tokenData;
-          }
-          throw new Error('Token is not active');
-        })
-        .catch((err) => {
-          throw err;
-        });
+    // Verification method order: static JWKS -> remote JWKS -> remote introspection
+    let verifiedToken = null;
+    if (options.jwks) {
+      debug('Using static JWKS to introspect token');
+      verifiedToken = await tryLocalIntrospect(options.jwks.keys);
+    }
+
+    if (!verifiedToken && fetchJwks) {
+      debug('Using remote JWKS to introspect token');
+      verifiedToken = await tryLocalIntrospect(await fetchJwks());
+    }
+
+    if (verifiedToken) {
+      return verifiedToken;
+    }
+
+    if (options.endpoint) {
+      debug('Doing remote introspection');
+      return remoteIntrospect(remoteIntrospectionInfo, token, tokenTypeHint);
+    }
+
+    throw new Error('Token is not active');
   };
 }
 
--- test/introspection.test.js
+++ test/introspection.test.js
@@ -2,13 +2,31 @@ const assert = require('chai').assert;
 const expect = require('chai').expect;
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+const nock = require('nock');
 const TokenIntrospection = require('../index');
 
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+const pem2jwk = require('pem-jwk').pem2jwk;
+
 chai.use(chaiAsPromised);
+nock.disableNetConnect();
 
-describe('Token introspection', () => {
-  it('throws error if endpoint is missing', () => expect(() => { TokenIntrospection(); }).to.throw(Error, 'missing from configuration'));
+const keyId = 'test_key_id';
+function setupJwks() {
+  const publicKey = fs.readFileSync('./test/public.pem', 'ascii');
+  const jwk = pem2jwk(publicKey);
+  return { keys: [Object.assign({ kid: keyId }, jwk)] };
+}
+const privateKey = fs.readFileSync('./test/private.pem', 'ascii');
+const jwks = setupJwks(keyId);
+const jwksUri = 'http://example.com/jwks';
 
+describe('Configuration', () => {
+  it('throws error if required config is missing', () => expect(() => TokenIntrospection()).to.throw(Error, 'must be specified in the configuration'));
+});
+
+describe('Remote token introspection', () => {
   it('calls fetch with correct parameters', () => {
     const introspection = new TokenIntrospection({
       endpoint: 'http://example.com/oauth/introspection',
@@ -49,5 +67,91 @@ describe('Token introspection', () => {
       fetch: () => Promise.resolve({ json: () => ({ active: false }) }),
     });
     return expect(introspection('token', 'access_token')).to.be.rejectedWith(Error, 'Token is not active');
+  });
+});
+
+describe('Local token introspection with static JWKS', () => {
+  const introspection = new TokenIntrospection({
+    fetch: () => {
+      throw new Error('should not be called');
+    },
+    jwks,
+  });
+
+  it('does local introspection if JWKS is specified', () => {
+    const now = Date.now() / 1000;
+    const accessTokenClaims = { exp: now + 5 };
+    const accessToken = jwt.sign(accessTokenClaims, privateKey, { algorithm: 'RS256', keyid: keyId, noTimestamp: true });
+    return expect(introspection(accessToken, 'access_token')).to.eventually.deep.equal(Object.assign({ active: true }, accessTokenClaims));
+  });
+
+  it('finds matching key for token without kid', () => {
+    const keys = jwks.keys.slice();
+    const otherKey = { n: '5hkbMTpub6WuqITpPhQHr5nvYz1t7PUg8ph9DEi55TtJXUT46S6viY-lNpBdLCOWes3mDD0VWKyXMO9JAeB9nw', e: 'AQAB' };
+    keys.unshift(otherKey);
+    const instance = new TokenIntrospection({
+      fetch: () => {
+        throw new Error('should not be called');
+      },
+      jwks: { keys },
+    });
+
+    const now = Date.now() / 1000;
+    const accessTokenClaims = { exp: now + 5 };
+    const accessToken = jwt.sign(accessTokenClaims, privateKey, { algorithm: 'RS256', noTimestamp: true });
+    return expect(instance(accessToken, 'access_token')).to.eventually.deep.equal(Object.assign({ active: true }, accessTokenClaims));
+  });
+
+  it('rejects mismatching kid for static JWKS', () => {
+    const now = Date.now() / 1000;
+    const accessTokenClaims = { exp: now + 5 };
+    const accessToken = jwt.sign(accessTokenClaims, privateKey, { algorithm: 'RS256', keyid: 'other_key_id' });
+    return expect(introspection(accessToken, 'access_token')).to.be.rejectedWith(Error, 'Token is not active');
+  });
+
+  it('rejects other token type than access_token', () => expect(introspection('foobar', 'other_token')).to.be.rejectedWith(Error, 'Token is not active'));
+
+  it('rejects expired token', () => {
+    const before = (Date.now() / 1000) - 1000;
+    const accessTokenClaims = { iat: before, exp: before + 5 };
+    const accessToken = jwt.sign(accessTokenClaims, privateKey, { algorithm: 'RS256' });
+    return expect(introspection(accessToken, 'access_token')).to.be.rejectedWith(Error, 'Token is not active');
+  });
+
+  it('rejects non-jwt token', () => expect(introspection('foobar', 'access_token')).to.be.rejectedWith(Error, 'Token is not active'));
+});
+
+describe('Local token introspection with remote JWKS', () => {
+  const introspection = new TokenIntrospection({
+    jwks_uri: jwksUri,
+    fetch: () => { throw new Error('should not be called'); },
+  });
+
+  it('does local introspection if JWKS uri is specified', () => {
+    nock('http://example.com')
+      .get('/jwks')
+      .reply(200, JSON.stringify(jwks));
+    const now = Date.now() / 1000;
+    const accessTokenClaims = { exp: now + 5 };
+    const accessToken = jwt.sign(accessTokenClaims, privateKey, { algorithm: 'RS256', keyid: keyId, noTimestamp: true });
+    return expect(introspection(accessToken, 'access_token')).to.eventually.deep.equal(Object.assign({ active: true }, accessTokenClaims));
+  });
+});
+
+describe('Fallback order for introspection methods: local introspection with static JWKS -> local introspection with remote JWKS -> remote introspection', () => {
+  it('falls back to remote introspection if the verification with static JWKS and remote JWKS fails', () => {
+    nock('http://example.com')
+      .get('/jwks')
+      .reply(200, JSON.stringify({ keys: [] })); // no keys
+
+    const introspection = new TokenIntrospection({
+      jwks: {}, // no keys
+      jwks_uri: jwksUri,
+      endpoint: 'http://example.com/oauth/introspection',
+      client_id: 'client',
+      client_secret: 'secret',
+      fetch: () => Promise.resolve({ json: () => ({ active: true }) }),
+    });
+    return expect(introspection('token', 'access_token')).to.eventually.deep.equal({ active: true });
   });
 });
