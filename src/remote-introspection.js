df27cff87a51a6c34fae677277c6663058e1ab89
Samuel Gulliksson <samuel.gulliksson@schibsted.com>
2017-10-09 10:40:45 +0200

    CR: Make remote introspection async.

--- src/remote-introspection.js
+++ src/remote-introspection.js
@@ -1,32 +1,34 @@
 const debug = require('debug')('token-introspection');
 const formEncode = require('form-urlencoded');
 
-function remoteIntrospect(requestInfo, token, tokenTypeHint) {
+async function remoteIntrospect(requestInfo, token, tokenTypeHint) {
   const data = { token };
   if (tokenTypeHint) {
     data.token_type_hint = tokenTypeHint;
   }
-
-  return requestInfo.fetch(requestInfo.endpoint, {
-    method: 'POST',
-    body: formEncode(data),
-    headers: {
-      Authorization: `Basic ${new Buffer(`${requestInfo.client_id}:${requestInfo.client_secret}`).toString('base64')}`,
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'User-Agent': requestInfo.user_agent,
-    },
-    agent: requestInfo.proxy,
-  })
-    .then(res => res.json())
-    .then((tokenData) => {
-      if (tokenData.active === true) {
-        return tokenData;
-      }
-      throw new Error('Token is not active');
-    })
-    .catch((err) => {
-      throw err;
+  
+  let res;
+  try {
+    res = await requestInfo.fetch(requestInfo.endpoint, {
+      method: 'POST',
+      body: formEncode(data),
+      headers: {
+        Authorization: `Basic ${new Buffer(`${requestInfo.client_id}:${requestInfo.client_secret}`).toString('base64')}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent': requestInfo.user_agent,
+      },
+      agent: requestInfo.proxy,
     });
+  } catch (err) {
+    debug('Remote token introspection request failed: ' + err.message);
+    throw new Error('Remote introspection request failed');
+  }
+
+  const tokenData = res.json();
+  if (tokenData.active === true) {
+    return tokenData;
+  }
+  throw new Error('Token is not active');
 }
 
 module.exports = remoteIntrospect;
