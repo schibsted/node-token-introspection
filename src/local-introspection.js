cb70a020c653b9d740ff80b63ce2466416acc2b3
Samuel Gulliksson <samuel.gulliksson@schibsted.com>
2017-10-09 09:32:23 +0200

    CR Refactor local introspection module.

--- src/local-introspection.js
+++ src/local-introspection.js
@@ -6,35 +6,6 @@ const promisify = require('util.promisify');
 const jwtVerify = promisify(jwt.verify);
 
 async function localIntrospect(keys, allowedAlgorithms, token, tokenTypeHint) {
-  function findCandidateKeys(jwtHeader) {
-    function alg2keyType(alg) {
-      if (alg.startsWith('HS')) {
-        return 'oct';
-      } else if (alg.startsWith('RS') || alg.startsWith('PS')) {
-        return 'RSA';
-      } else if (alg.startsWith('ES')) {
-        return 'EC';
-      }
-      return null;
-    }
-
-    const filteredKeys = keys.slice()
-      .filter(key => key.kty && key.kty === alg2keyType(jwtHeader.alg));
-    debug('Filtered keys for \'%s\', found %d', jwtHeader.alg, filteredKeys.length);
-
-    if (jwtHeader.kid) {
-      const keyWithKeyId = filteredKeys.find(key => key.kid === jwtHeader.kid);
-      if (keyWithKeyId) {
-        debug('Found key for key id %s', jwtHeader.kid);
-        return [keyWithKeyId];
-      }
-      debug('No key found for key id %s', jwtHeader.kid);
-      return [];
-    }
-
-    return filteredKeys;
-  }
-
   if (tokenTypeHint !== 'access_token') {
     debug('Not an access token, tokenTypeHint=%s', tokenTypeHint);
     throw new Error('Only access tokens are supported for local introspection');
@@ -46,7 +17,7 @@ async function localIntrospect(keys, allowedAlgorithms, token, tokenTypeHint) {
     throw new Error('Token is not a JWT');
   }
 
-  const possibleVerificationKeys = findCandidateKeys(decodedToken.header)
+  const possibleVerificationKeys = findCandidateKeys(decodedToken.header, keys)
     .map(jwk2pem);
 
   /* eslint-disable no-restricted-syntax, no-await-in-loop */
@@ -63,6 +34,35 @@ async function localIntrospect(keys, allowedAlgorithms, token, tokenTypeHint) {
   /* eslint-enable */
 
   throw new Error('Could not verify token with any key');
+}
+
+function findCandidateKeys(jwtHeader, keys) {
+  function alg2keyType(alg) {
+    if (alg.startsWith('HS')) {
+      return 'oct';
+    } else if (alg.startsWith('RS') || alg.startsWith('PS')) {
+      return 'RSA';
+    } else if (alg.startsWith('ES')) {
+      return 'EC';
+    }
+    return null;
+  }
+
+  const filteredKeys = keys.slice()
+    .filter(key => key.kty && key.kty === alg2keyType(jwtHeader.alg));
+  debug('Filtered keys for \'%s\', found %d', jwtHeader.alg, filteredKeys.length);
+
+  if (jwtHeader.kid) {
+    const keyWithKeyId = filteredKeys.find(key => key.kid === jwtHeader.kid);
+    if (keyWithKeyId) {
+      debug('Found key for key id %s', jwtHeader.kid);
+      return [keyWithKeyId];
+    }
+    debug('No key found for key id %s', jwtHeader.kid);
+    return [];
+  }
+
+  return filteredKeys;
 }
 
 module.exports = localIntrospect;
