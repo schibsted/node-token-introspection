5d2b5c60a5ae543c3bb82239bd2b76d7105c29f4
Adam Bukowski <bukowski.adam@gmail.com>
2024-04-17 13:59:38 +0200

    style: fix eslint errors

--- src/local-introspection.js
+++ src/local-introspection.js
@@ -32,7 +32,11 @@ module.exports = (options) => {
       rateLimit: options.jwks_ratelimit_enabled || true,
       jwksRequestsPerMinute: options.jwks_ratelimit_per_minute || 60, // 1 rps
       jwksUri: options.jwks_uri,
-      fetcher: options.fetch && ((url) => options.fetch(url).then(res => res.ok ? res.json() : Promise.reject(new Error(res.statusText)))),
+      fetcher: options.fetch && (
+        (url) => options.fetch(url).then(
+          (res) => (res.ok ? res.json() : Promise.reject(new Error(res.statusText))),
+        )
+      ),
     });
   }
 
